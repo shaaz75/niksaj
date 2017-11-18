06cdcb487fbf6dff3bfdc086f9ba3add23183de1
Sajid Ahmed Shahsroha <sajidahmed.77@gmail.com>
2017-11-18 22:10:30 +0400

    dfdf

---
+++ niksaj/niksaj/Sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace niksaj
+{
+    public class Sum
+    {
+        public   int Add()
+        {
+            int first = 1;
+            int second = 2;
+            return first + second;
+        }
+    }
+}
